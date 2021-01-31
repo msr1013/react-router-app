991b16bdde06d57e7e47ef838df1d22cabb7fef9
msr1013 <rawalsinghmanish@gmail.com>
2021-01-31 16:12:34 +0545

    navbar-style-changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,21 +28,24 @@ class NavBar extends React.Component {
 
         return (
             <Navbar color="dark" dark expand="md">
-                <NavbarBrand href="/">React Basics</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="mr-auto w-75" navbar>
-                        <NavItem>
-                            <Link to="/posts/"><NavLink>Posts</NavLink></Link>
-                        </NavItem>
+                <div className="container">
+                    <NavbarBrand href="/">React Basics</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto w-75" navbar>
+                            <NavItem>
+                                <Link to="/posts/"><NavLink>Posts</NavLink></Link>
+                            </NavItem>
 
-                        <NavItem>
-                            <Link to="/users"><NavLink>Users</NavLink></Link>
-                        </NavItem>
+                            <NavItem>
+                                <Link to="/users"><NavLink>Users</NavLink></Link>
+                            </NavItem>
 
-                    </Nav>
-                    <NavbarText>Logout</NavbarText>
-                </Collapse>
+                        </Nav>
+                        <NavbarText>Logout</NavbarText>
+                    </Collapse>
+                </div>
+
             </Navbar>
         )
     }
---
+++ src/pages/Users.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Users extends React.Component {
+    render() {
+        return <h1>This is a users page</h1>
+    }
+}
+
+export default Users
